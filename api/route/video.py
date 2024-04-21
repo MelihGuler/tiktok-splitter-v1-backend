f71418e72fee8e1636fb2c01864d93d47bd36227
MELİH GÜLER <melihguler999@gmail.com>
2024-04-21 13:14:29 +0300

    Add split route

--- api/route/video.py
+++ api/route/video.py
@@ -25,3 +25,22 @@ def get_tiktok_video():
     # Call the service method to get the video
     # file deepcode ignore XSS: <User input is sanitized above, with schema.load function >
     return video_service.get(request_body['video_url'])
+
+@video_api.route('/tiktok/split', methods=['POST'])
+def split_tiktok_video():
+    schema = VideoSchema(unknown=EXCLUDE)
+    
+    try:
+        # Validate request body against invalid schema data types
+        request_body = schema.load(request.json)
+    except ValidationError as err:
+        print(err.messages.get('video_url'))
+        # Return a nice message if validation fails
+        return jsonify({'error': err.messages}), HTTPStatus.BAD_REQUEST
+    
+    video_service = VideoService()
+
+    
+    # Call the service method to get the video
+    # file deepcode ignore XSS: <User input is sanitized above, with schema.load function >
+    return video_service.split(request_body['video_url'], request_body['part_length'])
--- api/service/video.py
+++ api/service/video.py
@@ -4,6 +4,7 @@ from api.constants.tiktok import BROWSER_NAME
 from flask import Response
 from http import HTTPStatus
 from api.constants.errors import VIDEO_COULDNT_FETCH
+from api.util.split import split_video
 
 class VideoService:
     def get(self, video_url):
@@ -17,5 +18,13 @@ class VideoService:
                       content_type='video/mp4', direct_passthrough=True)
             return final_response
     
-    def split(self, video_url):
-        return self.video_repository.split(video_url)
+    def split(self, video_url, part_length):
+        video = tiktok.save_tiktok(video_url, BROWSER_NAME)
+
+        if video is None:
+            return Response(VIDEO_COULDNT_FETCH, HTTPStatus.INTERNAL_SERVER_ERROR)
+        
+        else:
+            parts = split_video(video, part_length)
+            print(parts)
+            return parts
